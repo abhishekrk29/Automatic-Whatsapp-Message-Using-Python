64ee261e6f9f181c2d5775c8c33eda4765a8dbf8
Abhishek Rajurkar <55060501+abhishekrk29@users.noreply.github.com>
2021-01-22 01:45:07 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,10 @@
+import pywhatkit # importing module to call funtion
+
+#syntax for sending msg 
+#pywhatkit.sendwhatmsg('mobile number with country code','some message to send','time in hour','time in minute')
+pywhatkit.sendwhatmsg('+917000000000','Hello',1,36)
+
+#after above line webwhatsapp will open and automatically send the msg
+
+#this library can be used for various purpose
+# https://pypi.org/project/pywhatkit/
